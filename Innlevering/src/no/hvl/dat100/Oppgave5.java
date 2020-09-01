ec6b27ffdf94c442ff73c60b2c773365db64ea30
Moen <182908@stud.hvl.no>
2020-09-01 20:43:29 +0200

    Endret på print meldingen slik at studentene blir nummerert

--- Innlevering/src/no/hvl/dat100/Oppgave5.java
+++ Innlevering/src/no/hvl/dat100/Oppgave5.java
@@ -9,21 +9,17 @@ public class Oppgave5 {
 	}
 	
 	public static void read_n_scores(Integer n) {
-		for(int i = 0; i < n; i++) {
+		for(int i = 1; i <= n; i++) {
 			int score = 0;
 			
 			do {
-				if(score == 0) {
-					score = Integer.parseInt(
-							JOptionPane.showInputDialog("What is your score? (Only values between 0 and 100 are valid)"));
-				} 
-				else {
-					score = Integer.parseInt(
-							JOptionPane.showInputDialog("You entered an incorrect score, please enter a score between 0 and 100."));
-				}
+				if(score == 0)
+					score = Integer.parseInt(JOptionPane.showInputDialog("What is your score? (Only values between 0 and 100 are valid)"));
+				else 
+					score = Integer.parseInt(JOptionPane.showInputDialog("You entered an incorrect score, please enter a score between 0 and 100."));
 			} while (score < 0 || score > 100);
 			
-			System.out.println(get_grade(score));
+			System.out.println("Student nr " + i + ": " +  get_grade(score));
 		}
 	}
 	
@@ -33,10 +29,10 @@ public class Oppgave5 {
 		char[] grades = {'F', 'E', 'D', 'C', 'B', 'A'};
 		char grade = 'G';
 		
-		for(int i = 0 ; i < ranges.length; i++) {
+		for(int i = 0 ; i < ranges.length; i++)
 			if(score >= ranges[i])
 				grade = grades[i];
-		}
+		
 		return grade;
 	}
 }
